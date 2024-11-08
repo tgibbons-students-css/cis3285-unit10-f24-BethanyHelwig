53033d56a3131f851fad0d584dd715cb7aeeb911
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2024-11-08 15:16:42 -0600

    Learning Activity parts 1 and 2

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+
+
+namespace SingleResponsibilityPrinciple
+{
+    internal class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        ITradeDataProvider dataProvider;
+
+        public AdjustTradeDataProvider(ITradeDataProvider dataProvider) { 
+            this.dataProvider = dataProvider;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            IEnumerable<string> tradeData = dataProvider.GetTradeData();
+
+            List<string> lines = new List<string>();
+            foreach (string data in tradeData)
+            {
+                lines.Add(data.Replace("GBP", "EUR"));
+            }
+
+            return lines;
+        }
+    }
+}
